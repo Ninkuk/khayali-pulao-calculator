7d4063ebbebb00e221e837e6115a0b4e6281fdb2
Ninad Kulkarni <ninadk.dev@gmail.com>
2024-06-04 18:21:55 -0700

    made responsive and fixed validation

--- src/validation.js
+++ src/validation.js
@@ -7,29 +7,27 @@ const profitsInput = document.getElementById("profits");
 const unitsInput = document.getElementById("units");
 const sharesInput = document.getElementById("shares");
 
-profitsInput.addEventListener("input", (e) => {
-	// Check if input is empty or 0 and toggle red border
+const validateField = (e) => {
 	if (e.target.value === "" || e.target.value === "0") {
 		e.target.classList.add("border-red-500");
+		e.target.classList.remove("border-slate-700");
 	} else {
+		e.target.classList.add("border-slate-700");
 		e.target.classList.remove("border-red-500");
 	}
+};
+
+profitsInput.addEventListener("input", (e) => {
+	// Check if input is empty or 0 and toggle red border
+	validateField(e);
 });
 
 unitsInput.addEventListener("input", (e) => {
 	// Check if input is empty or 0 and toggle red border
-	if (e.target.value === "" || e.target.value === "0") {
-		e.target.classList.add("border-red-500");
-	} else {
-		e.target.classList.remove("border-red-500");
-	}
+	validateField(e);
 });
 
 sharesInput.addEventListener("input", (e) => {
 	// Check if input is empty or 0 and toggle red border
-	if (e.target.value === "" || e.target.value === "0") {
-		e.target.classList.add("border-red-500");
-	} else {
-		e.target.classList.remove("border-red-500");
-	}
+	validateField(e);
 });
